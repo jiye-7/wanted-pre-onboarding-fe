dc8bafcc2699d0d4e9510b9f696ceb106461b5d0
jiye-7 <jy522hi@gmail.com>
2022-08-22 20:49:45 +0900

    add: redux setting

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { applyMiddleware, legacy_createStore as createStore } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import promiseMiddleware from 'redux-promise';
+import rootReducer from './redux/reducers';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(promiseMiddleware, thunkMiddleware)));
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<Router>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</Router>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
