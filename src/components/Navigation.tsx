983f5aeeffe43ec7e2e9f75dfb77d647dcaedfb4
jiye-7 <jy522hi@gmail.com>
2022-08-23 20:24:37 +0900

    refactor: gnb code modify

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,14 +1,37 @@
-import React from 'react';
+import { useState } from 'react';
 import { Box, Tabs, Tab } from '@mui/material';
 
+interface LinkTabProps {
+	label?: string;
+	href?: string;
+}
+
+function LinkTab(props: LinkTabProps) {
+	return (
+		<Tab
+			component="a"
+			onClick={(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+				event.preventDefault();
+			}}
+			{...props}
+		/>
+	);
+}
+
 const Navigation = () => {
+	const [value, setValue] = useState<number>(0);
+
+	const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+		setValue(newValue);
+	};
+
 	return (
 		<nav>
 			<Box sx={{ width: '100%' }}>
-				<Tabs aria-label="nav tabs example">
-					<Tab label="My ToDo List :)" href="/" />
-					<Tab label="SignUp" href="/signup" />
-					<Tab label="SignIn" href="/signin" />
+				<Tabs value={value} onChange={handleChange} aria-label="Navigation Tabs">
+					<LinkTab label="My ToDo List :)" href="/" />
+					<LinkTab label="SignUp" href="/signup" />
+					<LinkTab label="SignIn" href="/signin" />
 				</Tabs>
 			</Box>
 		</nav>
