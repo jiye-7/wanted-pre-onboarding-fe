6b16024163cfadece560437787a3fda1219b2047
jiye-7 <jy522hi@gmail.com>
2022-08-24 18:53:03 +0900

    style: Todos component

--- src/components/Todo/Todos.tsx
+++ src/components/Todo/Todos.tsx
@@ -3,6 +3,23 @@ import { useDispatch } from 'react-redux';
 import { IResponseTodo } from '../../@types/types';
 import { createTodo, getTodos } from '../../redux/actions/todoAction';
 import Todo from './Todo';
+import { Container, Input } from '../SignIn/SignIn';
+import styled from 'styled-components';
+
+const WriteBox = styled.div`
+	display: 'flex';
+	flexdirection: 'row';
+	justifycontent: 'center';
+	alignitems: 'center';
+`;
+
+const Button = styled.button`
+	width: 4rem;
+	padding: 0.5rem;
+	margin: 0.5rem 0 0 2rem;
+	font-size: 1rem;
+	font-family: bole;
+`;
 
 const Todos = () => {
 	const dispatch = useDispatch();
@@ -43,17 +60,17 @@ const Todos = () => {
 	};
 
 	return (
-		<div>
-			<h3>{todos.length === 0 && '데이터를 가져오는 중입니다.. :)'}</h3>
+		<Container>
+			{<h4>{todos.length === 0 && '작성한 글들이 없습니다... :)'}</h4>}
 			{todos?.map((todo: IResponseTodo) => (
 				<Todo key={todo.id} todo={todo} />
 			))}
 			<h3>등록할 내용을 입력해주세요 :)</h3>
-			<div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
-				<input type="text" id="addtodo" value={todoValue} onChange={(e) => handleChangeInputValue(e)} />
-				<button onClick={handleAddTodo}>추가</button>
-			</div>
-		</div>
+			<WriteBox>
+				<Input type="text" id="addtodo" value={todoValue} onChange={(e) => handleChangeInputValue(e)} />
+				<Button onClick={handleAddTodo}>추가</Button>
+			</WriteBox>
+		</Container>
 	);
 };
 
